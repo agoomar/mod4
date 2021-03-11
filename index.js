53ab41b9854e410470b1b875c1372ebe7203fef5
agoomar <agoomar1996@gmail.com>
2021-03-11 14:30:21 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('test')
+require('dotenv').config()
+const express = require('express')
+// const path = require('path')
+
+const server = express()
+server.use(express.json())
+
+if (process.env.NODE_ENV === 'development') { 
+    const cors = require('cors')
+    server.use(cors())
+}
+
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'Testing' })
+})
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log (`Listening on http://localhost:${PORT}`)
+})
